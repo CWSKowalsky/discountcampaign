59b1225984fd9fb082d8e43ae5dfd2d7796127d2
SKowalsky <skowalsky37@gmail.com>
2018-07-03 14:46:23 +0200

    added english

---
+++ src/Resources/contao/languages/en/tl_discountcampaign.php
@@ -0,0 +1,39 @@
+<?php
+    $GLOBALS['TL_LANG']['tl_discountcampaign']['title_legend'] = 'Title';
+    
+    $GLOBALS['TL_LANG']['tl_discountcampaign']['title'][0] = 'Title';
+    $GLOBALS['TL_LANG']['tl_discountcampaign']['title'][1] = 'Enter the title of the discount campaign.';
+    
+    $GLOBALS['TL_LANG']['tl_discountcampaign']['discountcampaign_legend'] = 'Discount Campaigns';
+    
+    $GLOBALS['TL_LANG']['tl_discountcampaign']['products'][0] = 'Products';
+    $GLOBALS['TL_LANG']['tl_discountcampaign']['products'][1] = 'Add products to your discount campaign';
+
+    $GLOBALS['TL_LANG']['tl_discountcampaign']['lsShopProductSaving'][0] = 'Discouunt';
+    $GLOBALS['TL_LANG']['tl_discountcampaign']['lsShopProductSaving'][1] = 'The discount value, to be applied on the products';
+
+    $GLOBALS['TL_LANG']['tl_discountcampaign']['lsShopProductSavingType'][0] = 'Discount type';
+    $GLOBALS['TL_LANG']['tl_discountcampaign']['lsShopProductSavingType'][1] = 'Discount value in fixed values or percent';
+
+    $GLOBALS['TL_LANG']['tl_discountcampaign']['start'][0] = 'Valid from';
+    $GLOBALS['TL_LANG']['tl_discountcampaign']['stop'][0] = 'Valid to';
+    
+    $GLOBALS['TL_LANG']['tl_discountcampaign']['lsShopProductIsOnSale'][0] = 'Is on sale';
+    
+    $GLOBALS['TL_LANG']['tl_discountcampaign']['new'][0] = 'New';
+    $GLOBALS['TL_LANG']['tl_discountcampaign']['new'][1] = 'Create a new discount campaign';
+    
+    $GLOBALS['TL_LANG']['tl_discountcampaign']['edit'][0] = 'Edit';
+    $GLOBALS['TL_LANG']['tl_discountcampaign']['edit'][1] = 'Edit Discount Campaign ID %s';
+    
+    $GLOBALS['TL_LANG']['tl_discountcampaign']['delete'][0] = 'Delete';
+    $GLOBALS['TL_LANG']['tl_discountcampaign']['delete'][1] = 'Delete Discount Campaign ID %s';
+    
+    $GLOBALS['TL_LANG']['tl_discountcampaign']['show'][0] = 'Discount Campaign Details';
+    $GLOBALS['TL_LANG']['tl_discountcampaign']['show'][1] = 'Show details for the discount campaign with the ID %s';
+
+    $GLOBALS['TL_LANG']['tl_discountcampaign']['options']['savingType'] = array(
+      'savingTypeAbsolute' => array('Fixed value', 'The entered value will be subtracted from the product price'),
+      'savingTypePercentaged' => array('Percent value', 'The price of the product will be reduced by the given percent value.')
+	  );
+?>
