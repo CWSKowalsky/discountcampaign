b39495c2ae5fa25e2b22270b5a725a858083ed6a
SKowalsky <skowalsky37@gmail.com>
2018-07-03 15:08:17 +0200

    updated languages

---
+++ src/Resources/contao/languages/de/modules.php
@@ -0,0 +1,6 @@
+<?php
+
+$GLOBALS['TL_LANG']['MOD']['discountcampaign'][0] = 'Rabatte';
+$GLOBALS['TL_LANG']['MOD']['discountcampaign'][1] = 'Rabatte verwalten';
+$GLOBALS['TL_LANG']['FMD']['discountcampaign_list'][0] = 'Rabatte Liste';
+$GLOBALS['TL_LANG']['FMD']['discountcampaign_list'][1] = 'Auflistung der Rabatte';
---
+++ src/Resources/contao/languages/en/modules.php
@@ -0,0 +1,6 @@
+<?php
+
+$GLOBALS['TL_LANG']['MOD']['discountcampaign'][0] = 'Discount Campaigns';
+$GLOBALS['TL_LANG']['MOD']['discountcampaign'][1] = 'Manage Discount Campaigns';
+$GLOBALS['TL_LANG']['FMD']['discountcampaign_list'][0] = 'Discount Campaign List';
+$GLOBALS['TL_LANG']['FMD']['discountcampaign_list'][1] = 'List of all discount campaigns';
